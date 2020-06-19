91779def7268af6eaf0a30de12922e81fc88512d
Aurelien James <apf.james@gmail.com>
2020-06-19 21:02:36 +0200

    Implementation of home and camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,32 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
+import { CameraComponent } from './camera/camera.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    ZXingScannerModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   exports: [
-    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
