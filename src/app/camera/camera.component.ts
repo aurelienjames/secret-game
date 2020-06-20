536a48036009b9f5230f61abc0f2de122e718ccd
Aurelien James <apf.james@gmail.com>
2020-06-20 12:46:38 +0200

    Improve ui

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-camera',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraComponent implements OnInit {
 
+  error: boolean;
+  success: boolean;
   qrResultString: string;
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -17,10 +20,52 @@ export class CameraComponent implements OnInit {
 
   clearResult(): void {
     this.qrResultString = null;
+    this.error = false;
+    this.success = false;
   }
 
-  onCodeResult(resultString: string) {
+  scanSuccessHandler(resultString: string) {
+    console.log('scanSuccessHandler: ', resultString);
     this.qrResultString = resultString;
+    this.error = false;
+    this.success = true;
+    this.openSnackBarResult(resultString);
+  }
+
+  scanErrorHandler(event) {
+    console.log('scanErrorHandler: ');   
+    this.error = true;
+    this.success = false;
+  }
+
+  scanFailureHandler(event) {
+    console.log('scanFailureHandler: ');
+    this.error = true;
+    this.success = false;
+  }
+
+  scanCompleteHandler(event) {
+    console.log('scanCompleteHandler: ');
+  }
+
+  onTorchCompatible(event) {
+    console.log('onTorchCompatible: ');
+  }
+
+  camerasFoundHandler(event) {
+    console.log('camerasFoundHandler: ');
+  }
+
+  camerasNotFoundHandler(event) {
+    console.log('camerasNotFoundHandler: ');
+  }
+
+  openSnackBarResult(result: string) {
+    this._snackBar.open(`QRCODE succes !result: ${result}`, 'Close', {
+      duration: 4000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
   }
 
 }
