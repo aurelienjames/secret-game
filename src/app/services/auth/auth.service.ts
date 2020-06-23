80b072860e115b85ecd797fda3e12e4e6dbf460b
Aurelien James <apf.james@gmail.com>
2020-06-23 22:04:50 +0200

    Add signIn/signUp/signOut in the auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userData: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) {
+    this.userData = angularFireAuth.authState;
+  }
+
+  /* Sign up */
+  signUp(email: string, password: string) {
+    this.angularFireAuth.createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('Successfully signed up!', res);
+      })
+      .catch(error => {
+        console.log('Something is wrong:', error.message);
+      });    
+  }
+  
+  /* Sign in */
+  signIn(email: string, password: string) {
+    this.angularFireAuth.signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('Successfully signed in!');
+      })
+      .catch(err => {
+        console.log('Something is wrong:',err.message);
+      });
+  }
+  
+  /* Sign out */
+  signOut() {
+    this.angularFireAuth.signOut();
+  }  
 }
